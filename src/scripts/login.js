bbaaf147522e9c2fb405ee16af6be0a5f04732ef
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-05-22 17:31:10 -0300

    feat: Add login functionality and script for login page

---
+++ src/scripts/login.js
@@ -0,0 +1,18 @@
+import { getData, hashPassword } from './utils.js';
+
+const login = async (e) => {
+  let inputEmail = document.querySelector('#exampleInputEmail1').value;
+  let inputPassword = document.querySelector('#exampleInputPassword1').value;
+
+  const password = getData(inputEmail);
+
+  if (!password) return false;
+
+  const hashedInput = await hashPassword(inputPassword);
+  if (hashedInput !== password) return false;
+
+  window.open('dashboard.html', '_self');
+};
+
+const loginBtn = document.querySelector('.login-btn');
+loginBtn.addEventListener('click', login);
