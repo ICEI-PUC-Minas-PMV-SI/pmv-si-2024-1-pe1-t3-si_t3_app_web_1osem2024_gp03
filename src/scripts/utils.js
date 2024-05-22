c3fb3f31e34d212a585ca1d56f9b36b65db46167
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-05-22 17:29:44 -0300

    refactor: Update hashPassword function to use SHA-256 encryption algorithm

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+// import bcrypt from 'bcrypt';
 
 export const getData = (key) => {
   return localStorage.getItem(key);
@@ -8,12 +9,22 @@ export const setData = (key, value) => {
   localStorage.setItem(key, value);
 };
 
-export const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-};
-
 export const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
 
+export const hashPasswordd = async (password) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password);
+  const hash = await crypto.subtle.digest('SHA-256', data);
+  return hexString(hash);
+};
+
+function hexString(buffer) {
+  const byteArray = new Uint8Array(buffer);
+  const hexCodes = [...byteArray].map((value) => {
+    const hexCode = value.toString(16);
+    return hexCode.padStart(2, '0');
+  });
+  return hexCodes.join('');
+}
